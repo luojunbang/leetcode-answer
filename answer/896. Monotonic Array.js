247b03225e07cbe89c97222e97d9efefeb15fd06
luojunbang <luojunbang@foxmail.com>
2020-12-09 17:52:44 +0800

    896

--- answer/896. Monotonic Array.js
+++ answer/896. Monotonic Array.js
@@ -3,12 +3,17 @@
  * @return {boolean}
  */
 var isMonotonic = function (A) {
-    return A[0] == A[A.length - 1] ? !!A.some(i => i != A[0]) : A.some((item, idx) => A.slice(idx + 1).some((n, i) => A[0] < A[A.length - 1] ? n < item : n > item)
-    )
+    let curr = A[0], rise = A[0] > A[A.length - 1]
+    return A[0] == A[A.length - 1] ? !A.some(i => i != A[0]) : !A.some(i => {
+        let res = rise ? i > curr : i < curr
+        curr = i
+        return res
+    })
 };
 
 console.log(isMonotonic([6, 5, 7, 4]));
 console.log(isMonotonic([6, 5, 4, 4]));
+console.log(isMonotonic([4, 4, 4, 4]));
 // console.log(isMonotonic([1, 2, 2, 3]));
 // console.log(isMonotonic([1, 3, 2, 4]));
 // console.log(isMonotonic([1, 3, 2]));
